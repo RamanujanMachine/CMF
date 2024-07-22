6d352838762878e23c579c69e814538ecb0f482f
Tomer Raz <you@example.com>
2024-07-22 21:47:35 +0300

    Add doc strings and reformat tests

--- ramanujantools/pcf/interlaced_pcf.py
+++ ramanujantools/pcf/interlaced_pcf.py
@@ -1,61 +1,83 @@
 import sympy as sp
-from sympy.abc import n
 
-from typing import Dict, List, Collection
-from multimethod import multimethod
-
-from ramanujantools import Matrix, Limit
+from ramanujantools import Matrix
 from ramanujantools.pcf import PCF
 
 
 class InterlacedPCF(PCF):
     r"""
-    Represents Interlaced Polynomial Continued Fractions. These are PCFs constructed by partial 
+    Represents Interlaced Polynomial Continued Fractions. These are PCFs constructed by partial
     numerators and denominators that alternate between a few different polynomials.
-        
-    The polynomial matrix representation $M_n$ is given by the product of the 
+    An Interlaced PCF is not a PCF, but can be converted into one.
+
+    The polynomial matrix representation $M_n$ is given by the product of the
     matrices representing each numerator-denominator pair.
-    For example, consider a continued fraction with three sequences appearing as the partial 
-    numerator - $b_1, b_2, b_3$ - and two sequences appearing as the partial denominator 
-    - $a_1, a_2$:
-    The CF's matrix is represented by $M_n = \prod_{i=1}^6 \begin{pmatrix} 0 & b_i(n) \cr 1 & a_i(n) \end{pmatrix}$.
-    Where $6 = lcm(3, 2)$ is the period of the continued fraction and the sequences are repeated 
+    For example, consider a continued fraction with three polynomials appearing as the partial
+    numerator - $b_1, b_2, b_3$ - and two polynomials appearing as the partial denominator - $a_1, a_2$:
+    The Interlaced PCF is represented by
+    $M_n = \prod_{i=1}^6 \begin{pmatrix} 0 & b_i(n) \cr 1 & a_i(n) \end{pmatrix}$,
+    where $6 = lcm(3, 2)$ is the period of the continued fraction and the sequences of polynomials are repeated
     to match the period of the continued fraction.
     """
 
     def __init__(self, a0, a_dict, b_dict):
         """
-        Initializes an Interlaced PCF with
-        - a0: The free constant of the continued fraction.
-        - a_dict: A dictionary of polynomials representing `a_n`, the partial denominator of the continued fraction.
-        - b_dict: A dictionary of polynomials representing `b_n`, the partial numerator of the continued fraction.
-        Note: the order of the polynomials in the dictionaries is the order in which they will appear in the continued fraction.
+        Initializes an Interlaced PCF with a number `a0` and `a_dict`, and `b_dict`, dictionaries of polynomials.
+        For example: `inter_pcf = InterlacedPCF(3, {1: 3}, {1: -n, 2: n})`
+        Note: the order of the polynomials in the dictionaries is the order
+        in which they will appear in the continued fraction.
         """
         self.a0 = a0
+        """The free constant of the continued fraction."""
+
         self.a_dict = {f'a_{i+1}': val for (i, val) in enumerate(list(a_dict.values()))}
+        """A dictionary of polynomials representing `a_n`, the partial denominator of the continued fraction."""
+
         self.b_dict = {f'b_{i+1}': val for (i, val) in enumerate(list(b_dict.values()))}
+        """A dictionary of polynomials representing `b_n`, the partial numerator of the continued fraction."""
+
         self.period = sp.lcm(len(a_dict), len(b_dict))
+        """The lcm of the lengths of `a_dict` and `b_dict`."""
 
         self.a_list = list(a_dict.values()) * (self.period // len(a_dict))
+        """The partial denominators of the continued fraction repeated to match the period."""
+
         self.b_list = list(b_dict.values()) * (self.period // len(b_dict))
+        """The partial numerators of the continued fraction repeated to match the period."""
 
         M = Matrix.eye(2)
         for a, b in zip(self.a_list, self.b_list):
             M = M * Matrix([[0, b], [1, a]])
+
         self.M_mat = M
+        """The polynomial matrix representing the interlaced continued fraction."""
 
     def __repr__(self):
         return f"InterlacedPCF({self.a0}, {self.a_dict}, {self.b_dict})"
-    
+
     def __str__(self):
         return self.__repr__()
-    
-    def M(self):
-        return self.M_mat
-    
-    def A(self):
-        return Matrix([[1, self.a0], [0, 1]])
-    
-    def pcf(self):
-        return self.M_mat.as_pcf().pcf
 
+    def M(self):
+        r"""
+        Returns the matrix representation of the interlaced continued fraction.
+
+        $M = \prod\limits_{i=1}^p \begin{pmatrix} 0, b_i(n) \cr 1, a_i(n) \end{pmatrix}$
+        where $p$ is the period of the Interlaced PCF.
+        """
+        return self.M_mat
+
+    def A(self):
+        r"""
+        Returns the matrix that represents the $a_0$ part:
+
+        $A = \begin{pmatrix} 1, a_0 \cr 0, 1 \end{pmatrix}$
+        """
+        return Matrix([[1, self.a0], [0, 1]])
+
+    def pcf(self):
+        """
+        Returns the PCF representation of the interlaced continued fraction.
+        Note: an Interlaced PCF is not a PCF, but can be converted into one.
+        """
+        return self.M_mat.as_pcf().pcf
--- ramanujantools/pcf/interlaced_pcf_test.py
+++ ramanujantools/pcf/interlaced_pcf_test.py
@@ -6,23 +6,24 @@ from ramanujantools import Matrix
 
 def test_period():
     a0 = 1
-    a_dict = {1: 1, 2: 1, 3: 1}
-    b_dict = {1: 1, 2: 1}
+    a_dict = {'a1': 1, 'a2': -n, 'a3': 3 * n**3 - 5}
+    b_dict = {'b1': n, 'b2': 2 * n**2}
     pcf = InterlacedPCF(a0, a_dict, b_dict)
-    assert pcf.period == 6
+    assert 6 == pcf.period
 
 
-def test_a_list():
+def test_ab_lists():
     a0 = 1
-    a_dict = {1: 1, 2: 1, 3: 1}
-    b_dict = {1: 1, 2: 1}
+    a_dict = {'a1': 1, 'a2': -n, 'a3': 3 * n**3 - 5}
+    b_dict = {'b1': n, 'b2': 2 * n**2}
     pcf = InterlacedPCF(a0, a_dict, b_dict)
-    assert pcf.a_list == [1, 1, 1, 1, 1, 1]
+    assert [1, -n, 3 * n**3 - 5, 1, -n, 3 * n**3 - 5] == pcf.a_list
+    assert [n, 2 * n**2, n, 2 * n**2, n, 2 * n**2] == pcf.b_list
 
 
 def test_M():
-    m = InterlacedPCF(3, {1: 3}, {1: -n, 2: n}).M()
-    assert Matrix([[-n, -3 * n], [3, n + 9]]) == m
+    pcf = InterlacedPCF(3, {1: 3}, {1: -n, 2: n})
+    assert Matrix([[-n, -3 * n], [3, n + 9]]) == pcf.M()
 
 
 def test_pcf():
